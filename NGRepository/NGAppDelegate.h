e6e263d49292f6433c3552d99917c6c8f3aad03e
Andrei Puni <puni.andrei23@gmail.com>
2013-09-16 00:23:20 +0300

    Initial Commit

---
+++ NGRepository/NGAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  NGAppDelegate.h
+//  NGRepository
+//
+//  Created by Andrei on 9/16/13.
+//  Copyright (c) 2013 Andrei Puni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class NGViewController;
+
+@interface NGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) NGViewController *viewController;
+
+@end
