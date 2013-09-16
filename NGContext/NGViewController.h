afd011c4dc3a346d2883195fef19972025fa5a1a
Andrei Puni <puni.andrei23@gmail.com>
2013-09-16 22:00:26 +0300

    Initial Commit

---
+++ NGContext/NGViewController.h
@@ -0,0 +1,13 @@
+//
+//  NGViewController.h
+//  NGContext
+//
+//  Created by Andrei on 9/16/13.
+//  Copyright (c) 2013 Andrei Puni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NGViewController : UIViewController
+
+@end
---
+++ NGContext/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NGContext
+//
+//  Created by Andrei on 9/16/13.
+//  Copyright (c) 2013 Andrei Puni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NGAppDelegate class]));
+    }
+}
